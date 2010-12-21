d99e95aba9d9eafab81dffa60c944b7ad4497e04
Brian Getting <brian@terra-firma-design.com>
2010-12-20 19:51:25 -0800

    Adding needed methods to the blank slate

--- lib/hominid.rb
+++ lib/hominid.rb
@@ -10,7 +10,7 @@ module Hominid
   class API
     # Blank Slate
     instance_methods.each do |m|
-      undef_method m unless m.to_s =~ /^__|object_id|method_missing|respond_to?/
+      undef_method m unless m.to_s =~ /^__|object_id|method_missing|respond_to?|to_s|inspect/
     end
     
     include Hominid::Campaign
